5eed3eaf135aacbfaa68bafeb57c277f4f002526
Aamina Bibi <97452800+AAMINABIBI@users.noreply.github.com>
2022-06-11 18:57:13 +0500

    Add files via upload

---
+++ JavaScript1.js
@@ -0,0 +1,13 @@
+﻿
+    var string = "";
+function func1(num) {
+    if (num == '=') {
+        string = eval(string);
+        document.querySelector('input').value = string;
+    }
+    else {
+        string = string + num;
+        document.querySelector('input').value = string;
+    }
+}
+       
